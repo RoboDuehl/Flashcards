7017d6f9c065b5d19487c42e4334fc6edd393dee
awieland-can <andall@gmx.net>
2025-09-24 21:52:56 -0400

    updates

--- seed.py
+++ seed.py
@@ -6,9 +6,9 @@ data = [
     ("cogent", "clear, logical, and convincing", "Her argument was both cogent and compelling."),
 ]
 
-conn = sqlite3.connect('vocab.db')
-c = conn.cursor() # what is this doing ?
-c.execute("DROP TABLE IF EXISTS vocabulary")
+conn = sqlite3.connect('vocab.db') # makes the connection 
+c = conn.cursor() # creates the tunnel between the database and the program, opens the door so to say 
+c.execute("DROP TABLE IF EXISTS vocabulary") # commands similar to a CRUD method 
 c.execute(open("schema.sql").read())
 c.executemany("INSERT INTO vocabulary (word, meaning, example) VALUES (?, ?, ?)", data)
 conn.commit()
